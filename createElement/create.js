b5dff2b7f9c569b534134e116e65533abc5e82dc
kishorekumar <kishoremech2698@gmail.com>
2022-10-04 22:01:41 +0530

    first commit

---
+++ createElement/create.js
@@ -0,0 +1,35 @@
+const body =document.querySelector("body");
+//div1
+const div=document.createElement("div");
+div.className="container";
+body.append(div);
+
+
+
+
+
+//input value
+const inputdata=document.createElement("input");
+inputdata.className="ipdata";
+inputdata.type="text";
+inputdata.placeholder="type your goal";
+div.append(inputdata);
+
+// button
+const butt=document.createElement("button");
+butt.textContent="next";
+butt.className="btn";
+div.append(butt);
+
+//div2
+const div2=document.createElement("div");
+div2.id="change";
+body.appendChild(div2);
+
+const changes=document.getElementById("change")
+
+
+ butt.addEventListener('click',()=>{
+  changes.innerHTML=`<h3>${inputdata.value}</h3>`
+ 
+ })
